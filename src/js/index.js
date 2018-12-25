beba941db3663eafa0faa12b82958fec29645c90
Oleg Moshkovich <oleg.mosh@gmail.com>
2018-12-24 19:45:28 -0500

    added and defined the searchModel and searchView

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,30 @@
-import string from './models/Search'
+
+import Search from './models/Search';
+import * as searchView from './views/SearchView'
+import {elements} from './views/base'
+
+// const search = new Search('pizza');
+// search.getResults();
+
+let state = {};
+
+const controlSearch = async () =>{
+    // 1.  get the query from the view
+  const query = searchView.getInput();
+  console.log('this is the query' + query)
+  if(query){
+    // 2. New search object and add it to state
+    state.search = new Search(query) // store it in the global object
+  }
+    //3.Prepare UI interface for the results
+    //4. search for recipes
+    await state.search.getResults()
+    //5. render results on UI
+    console.log (state.search.result)
+}
+
+
+elements.searchForm.addEventListener('submit',(e)=>{
+  e.preventDefault();
+  controlSearch();
+})
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,16 @@
-export default 'I am an exported string'
+import axios from 'axios'
+
+export default class Search {
+  constructor(query){
+    this.query = query;
+  }
+  async getResults(){
+      const key = '98696078a29b431174ade4728478fd7c'
+      try{
+          const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`)
+          this.result = res.data.recipes  //becasue we want to save results from the query in the object itself
+      }catch(error) {
+          alert(error)
+      }
+  }
+}
